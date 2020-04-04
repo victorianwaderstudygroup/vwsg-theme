ee03203c6cb77a3860d6ce1030409d4d532ffbbf
Scott Limmer <scottlimmer@gmail.com>
2020-04-04 16:25:41 +1100

    style / responsive updates

--- front-page.php
+++ front-page.php
@@ -12,71 +12,73 @@
 get_header();
 
 ?>
-    <div class="row">
-        <img class="banner" src="<?= get_template_directory_uri() ?>/images/home-banner-1.jpg">
-    </div>
+	<div class="row">
+		<img class="banner" src="<?=get_template_directory_uri()?>/images/home-banner-1.jpg">
+	</div>
 
-    <div class="row features">
-        <?php
-        $i = 0;
-        $args = [
-            'category' => 6,
-            'numberposts' => 3,
-            'post_type' => 'page',
-            'orderby' => 'meta_value_num',
-            'meta_key' => 'home_position',
-            'order' => 'ASC'
-        ];
-        foreach (get_posts($args) as $post) : setup_postdata($post); ?>
-            <a class="col-xs-12 col-sm-6 col-md-4 feature" href="<?= the_permalink() ?>">
-                <?php if (has_post_thumbnail()) : ?>
-                    <img src="<?= the_post_thumbnail_url('75'); ?>" class="feature-img">
-                <?php
-                endif;
-                ?>
-                <span><?= the_title(); ?></span></a>
-        <?php
-        endforeach;
-        wp_reset_postdata();
-        ?>
-    </div>
+	<div class="row features">
+		<?php
+		$i = 0;
+		$args = [
+			'category' => 6,
+			'numberposts' => 3,
+			'post_type' => 'page',
+			'orderby' => 'meta_value_num',
+			'meta_key' => 'home_position',
+			'order' => 'ASC'
+		];
+		foreach (get_posts($args) as $post) : setup_postdata($post); ?>
+			<a class="col-xs-12 col-sm-6 col-md-4 feature" href="<?=the_permalink()?>">
+				<?php if (has_post_thumbnail()) : ?>
+					<img src="<?=the_post_thumbnail_url('75');?>" class="feature-img">
+				<?php
+				endif;
+				?>
+				<span><?=the_title();?></span></a>
+		<?php
+		endforeach;
+		wp_reset_postdata();
+		?>
+	</div>
 
-    <div class="row">
-        <div class="col-xs-12 col-md-8 col-lg-9 main">
-            <?php
-            if (have_posts()) :
-                while (have_posts()) : the_post(); ?>
-                    <h1><?php the_title() ?></h1>
-                    <?php
-                    the_content();
-                endwhile;
-            endif;
-            ?>
-        </div>
-        <div class="col-xs-12 col-md-4 col-lg-3 news section">
-            <h2>News</h2>
-            <ul>
-                <?php
-                foreach (get_posts() as $post) : setup_postdata($post); ?>
-                    <li class="news_item">
-                        <a href="<?php the_permalink() ?>">
-                            <!-- <div class="col-xs-3">
-                                        <span class="date"><? /*= get_the_date('d') */ ?><br><? /*= get_the_date('M') */ ?></span>
-                                    </div>-->
-                            <?php the_title() ?>
-                        </a>
-                    </li>
-                <?php endforeach;
-                wp_reset_postdata();
-                ?>
-            </ul>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12 tweets section">
-            <h2> Tweets</h2>
-            <?= display_tweets('<div class="col-xs-12 col-md-4">%s</div>') ?>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-xs-12 col-md-8 col-lg-8 main">
+			<?php
+			if (have_posts()) :
+				while (have_posts()) : the_post(); ?>
+					<h1><?php the_title() ?></h1>
+					<?php
+					the_content();
+				endwhile;
+			endif;
+			?>
+		</div>
+		<div class="col-xs-12 col-md-4 col-lg-4 news section">
+			<h2><i class="far fa-newspaper"></i> News</h2>
+			<?php
+			$args = [
+				'numberposts' => 1,
+				'post_type' => 'post',
+				'orderby' => 'date',
+				'order' => 'DESC'
+			];
+			foreach (get_posts($args) as $post) : setup_postdata($post); ?>
+				<div class="news_item">
+					<h3><?php the_title() ?></h3>
+					<?php the_excerpt(); ?>
+					<a href="<?php the_permalink() ?>">Continue reading <?php the_title() ?></a>
+				</div>
+			<?php endforeach;
+			wp_reset_postdata();
+			?>
+			<a class="more-news" href="/news-events">More news &rarr;</i></a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 tweets section">
+			<h2><i class="fab fa-twitter"></i> Tweets</h2>
+			<?=display_tweets('<div class="col-xs-12 col-md-4">%s</div>')?>
+		</div>
+	</div>
 <?php
 get_footer();
